df7a3d234c3d8317cecac5a9769264a3a762eecc
Marco <114123687+marcobustosblas@users.noreply.github.com>
2025-05-09 15:53:02 -0400

    Add welcome message to Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("¡Este es mi primer cambio en GitHub!"); // 👈 Nueva línea
     }
 }
